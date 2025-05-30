2b35feb126d05fb544c68185dd12d6fd6a8548fb
devguilara <fan3rtc@outlook.com>
2025-05-30 11:04:31 -0300

    first commit

---
+++ src/main/java/com/jornadacasamento/jornadadocasamento/JornadaDoCasamentoApplication.java
@@ -0,0 +1,13 @@
+package com.jornadacasamento.jornadadocasamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaDoCasamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JornadaDoCasamentoApplication.class, args);
+    }
+
+}
